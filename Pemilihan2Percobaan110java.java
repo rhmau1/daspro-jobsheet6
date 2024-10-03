b897645794850aa7a675e604ae6071fabf5bf512
rahma <fijriatirahmaturrizqi@gmail.com>
2024-10-03 08:24:22 +0700

    update: modifikasi kode program agar menghasilkan output yang sesuai

--- Pemilihan2Percobaan110java.java
+++ Pemilihan2Percobaan110java.java
@@ -6,11 +6,11 @@ public class Pemilihan2Percobaan110java {
         System.out.print("masukkan tahun yang ingin di cek: ");
         int tahun = input10.nextInt();
         
-        if((tahun % 4) == 0) {
-            if((tahun % 100) != 0)
-                System.out.println("tahun kabisat");                        
-        }else
+        if((tahun % 4 == 0) && (tahun % 100 != 0)){
+            System.out.println("tahun kabisat");            
+        }else{
             System.out.println("bukan tahun kabisat");
+        }
         
     }
 }
