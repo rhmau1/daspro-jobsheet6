2496cb937d3defc15f013283bf4308c103064eb5
rahma <fijriatirahmaturrizqi@gmail.com>
2024-10-03 08:39:06 +0700

    update: tambahan pengecualian tahun kelipatan 400

--- Pemilihan2Percobaan110java.java
+++ Pemilihan2Percobaan110java.java
@@ -1,16 +1,23 @@
 import java.util.Scanner;
 public class Pemilihan2Percobaan110java {
     public static void main(String[] args) {
-        Scanner input10 = new Scanner(System.in);
-        
+        Scanner input10 = new Scanner(System.in);        
         System.out.print("masukkan tahun yang ingin di cek: ");
-        int tahun = input10.nextInt();
-        
-        if((tahun % 4 == 0) && (tahun % 100 != 0)){
-            System.out.println("tahun kabisat");            
-        }else{
-            System.out.println("bukan tahun kabisat");
-        }
-        
+        int tahun = input10.nextInt();    
+        if (tahun % 400 == 0) {
+            System.out.println("Tahun kabisat");
+        } else {
+            // Cek apakah tahun habis dibagi 100
+            if (tahun % 100 == 0) {
+                System.out.println("Bukan tahun kabisat");
+            } else {
+                // Cek apakah tahun habis dibagi 4
+                if (tahun % 4 == 0) {
+                    System.out.println("Tahun kabisat");
+                } else {
+                    System.out.println("Bukan tahun kabisat");
+                }
+            }
+        }        
     }
 }
